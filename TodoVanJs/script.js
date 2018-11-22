179d9a3c946c9a798a8e2c8e92fca84491a08d10
Jaji <jajmantobi@gmail.com>
2018-11-22 10:51:39 +0100

    Add files via upload

---
+++ TodoVanJs/script.js
@@ -0,0 +1,52 @@
+var ul = document.getElementById('list');
+ var li;
+
+var addButton =document.getElementById('add');
+ addButton.addEventListener('click', addItem );
+
+ var removeButton = document.getElementById('remove');
+ removeButton.addEventListener('click', removeItem);
+
+
+ function addItem () {
+     var input = document.getElementById('input');
+     var item = input.value;
+     var text = document.createTextNode(item);
+
+
+     if (item === '') {
+         return false;
+         
+     } else {
+ //create li
+  li = document.createElement('li');
+
+//create input
+ var checkbox = document.createElement('input');
+ checkbox.type = 'checkbox';
+//  checkbox.setAttribute('id', 'check');
+
+//create label
+ var label = document.createElement('label');
+
+ //insert these elements
+ ul.appendChild(label);
+ li.appendChild(checkbox);
+ label.appendChild(text)
+ li.appendChild(label);
+ ul.insertBefore(li, ul.childNodes[0]);
+
+ input.value = '';
+
+     }
+}
+
+function removeItem() {
+    li = ul.children
+
+    for (let index = 0; index < li.length; index ++) {
+        while (li[index] && li[index].children[0].checked) {
+            ul.removeChild(li[index])
+        }
+    }
+}
